32b6819bb04a8e9dc0455d158a4635d63aa91ab9
tesla <tesla@LAPTOP-JH1GL291>
2023-01-11 21:28:19 +1000

    Добавьте файлы проекта.

---
+++ Project3/Project.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS//чтобы не добавлять _s к scanf
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <Windows.h>
+#pragma execution_character_set( "utf-8" )
+#include <time.h>
+
+//char display() {
+	//char a[100];
+	//printf("Введите логин: ");
+	//scanf("%s", a);
+//}
+int main() {
+	SetConsoleOutputCP(65001);
+
+	printf("                                                   ╔═══════════╗     ╔═══════════╗\n");
+	printf("                                                   ║Регистрация║     ║Авторизация║\n");
+	printf("                                                   ╚═══════════╝     ╚═══════════╝\n");
+	printf("                                        Нажмите O для регистрации или P для авторизации\n");
+	char a;
+	char login[100];
+	char password[100];
+	a = _getch();
+	//printf("%c", a);
+}
